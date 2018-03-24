44cba03867ece2ca446cf40c502640df796f2608
Muhammed Aydemir <muhammed.1299x@gmail.com>
2018-03-25 01:53:57 +0300

    Add eslint, Update sidemenu

--- src/sidemenu/index.js
+++ src/sidemenu/index.js
@@ -5,6 +5,8 @@ import Sidebar from 'react-sidebar'
 import MaterialTitlePanel from './material_title_panel'
 import SidebarContent from './sidebar_content'
 
+import './Sidemenu.css'
+
 const styles = {
   contentHeaderMenuLink: {
     textDecoration: 'none',
@@ -20,93 +22,46 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.renderPropCheckbox = this.renderPropCheckbox.bind(this)
-    this.renderPropNumber = this.renderPropNumber.bind(this)
     this.onSetOpen = this.onSetOpen.bind(this)
     this.menuButtonClick = this.menuButtonClick.bind(this)
-    this.closeDrawer = this.closeDrawer.bind(this)
+    this.onPressMenuItem = this.onPressMenuItem.bind(this)
 
     this.state = {
-      docked: false,
-      open: false,
+      docked: true,
+      open: true,
       transitions: true,
       touch: true,
       shadow: true,
       pullRight: false,
       touchHandleWidth: 20,
-      dragToggleDistance: 30
+      dragToggleDistance: 30,
+      navBarTitle: 'Ana Sayfa'
     }
   }
 
   onSetOpen(open) {
-    this.setState({ open: open })
+    this.setState({ open, docked: open })
   }
 
-  menuButtonClick(ev) {
-    ev.preventDefault()
+  onPressMenuItem = title =>
+    this.setState({ open: false, navBarTitle: title || this.state.navBarTitle, docked: false })
+
+  menuButtonClick() {
     this.onSetOpen(!this.state.open)
   }
 
-  renderPropCheckbox(prop) {
-    const toggleMethod = ev => {
-      const newState = {}
-      newState[prop] = ev.target.checked
-      this.setState(newState)
-    }
-
-    return (
-      <p key={prop}>
-        <input
-          type="checkbox"
-          onChange={toggleMethod}
-          checked={this.state[prop]}
-          id={prop}
-        />
-        <label htmlFor={prop}> {prop}</label>
-      </p>
-    )
-  }
-
-  renderPropNumber(prop) {
-    const setMethod = ev => {
-      const newState = {}
-      newState[prop] = parseInt(ev.target.value, 10)
-      this.setState(newState)
-    }
-
-    return (
-      <p key={prop}>
-        {prop}{' '}
-        <input type="number" onChange={setMethod} value={this.state[prop]} />
-      </p>
-    )
-  }
-
-  closeDrawer = () => {
-    this.setState({ open: false })
-  }
-
   render() {
-    const sidebar = <SidebarContent closeDrawer={this.closeDrawer} />
+    const sidebar = <SidebarContent onPressMenuItem={this.onPressMenuItem} />
 
     const contentHeader = (
       <span>
-        {!this.state.docked && (
-          <a
-            onClick={this.menuButtonClick}
-            href="#"
-            style={styles.contentHeaderMenuLink}
-          >
-            =
-          </a>
-        )}
-        <span> React Sidebar</span>
+        <span>{this.state.navBarTitle}</span>
       </span>
     )
 
     const sidebarProps = {
-      sidebar: sidebar,
-      docked: this.state.docked,
+      sidebar,
+      docked: false && this.state.docked,
       sidebarClassName: 'custom-sidebar-class',
       open: this.state.open,
       touch: this.state.touch,
@@ -121,6 +76,7 @@ export default class App extends React.Component {
     return (
       <Sidebar {...sidebarProps}>
         <MaterialTitlePanel title={contentHeader}>
+          <div className="sidemenu-button" onClick={this.menuButtonClick} />
           <div style={styles.content}>{this.props.children}</div>
         </MaterialTitlePanel>
       </Sidebar>
